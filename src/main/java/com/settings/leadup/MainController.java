df3a4e8a2aba339bedb06d62a61fbda7cc528857
VitalikoV <drap877@gmail.com>
2020-07-10 15:15:28 +0300

    design fixing

--- src/main/java/com/settings/leadup/MainController.java
+++ src/main/java/com/settings/leadup/MainController.java
@@ -1,7 +1,9 @@
 package com.settings.leadup;
 
+import com.settings.leadup.mail.EmailCfg;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -9,4 +11,10 @@ public class MainController {
     public String mainPage(){
         return "home";
     }
+
+    @PostMapping("/")
+    public void page(){
+
+    }
+
 }
--- src/main/java/com/settings/leadup/mail/FeedbackController.java
+++ src/main/java/com/settings/leadup/mail/FeedbackController.java
@@ -2,30 +2,30 @@ package com.settings.leadup.mail;
 
 import com.settings.leadup.mail.EmailCfg;
 import com.settings.leadup.mail.Feedback;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Properties;
 
 @RestController
-@RequestMapping("/feedback")
+@RequestMapping( value = "/feedback")
 public class FeedbackController {
 
     private EmailCfg emailCfg;
 
+    @Autowired
     public FeedbackController(EmailCfg emailCfg){
         this.emailCfg = emailCfg;
     }
 
     @PostMapping
     public void sendFeedback(@RequestBody Feedback feedback, BindingResult bindingResult) throws Exception {
-        if(bindingResult.hasErrors()){
-            throw new Exception("Feedback is not valid");
-        }
-
+//        if(bindingResult.hasErrors()){
+//            throw new Exception("Feedback is not valid");
+//        }
 
 
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -33,6 +33,12 @@ public class FeedbackController {
     mailSender.setPort(this.emailCfg.getPort());
     mailSender.setUsername(this.emailCfg.getUsername());
     mailSender.setPassword(this.emailCfg.getPassword());
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smth.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
 
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
